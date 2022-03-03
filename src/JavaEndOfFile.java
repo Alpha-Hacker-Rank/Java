77eb1142dfc020d3c2159400b28f022fc07fc8cf
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2022-03-03 08:53:57 +0530

    JavaEndOfFile

---
+++ src/JavaEndOfFile.java
@@ -0,0 +1,18 @@
+//Mindula Dilthushan
+//Hacker Rank - Java
+
+import java.util.*;
+
+public class JavaEndOfFile {
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+
+        Scanner scanner = new Scanner(System.in);
+        int i = 1;
+        while (scanner.hasNext()) {
+            String s = scanner.nextLine();
+            System.out.println(i + " " + s);
+            i++;
+        }
+    }
+}
