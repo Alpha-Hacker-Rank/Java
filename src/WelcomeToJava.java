60b961716b0e6e45f346ef50278c727a4d3fca53
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2022-03-04 09:49:50 +0530

    WelcomeToJava

---
+++ src/WelcomeToJava.java
@@ -0,0 +1,11 @@
+//Mindula Dilthushan
+//Hacker Rank - Java
+
+public class WelcomeToJava {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
