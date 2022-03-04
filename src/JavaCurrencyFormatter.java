6e71fe97cdc70fb43547bc42cd70808f22b773e9
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2022-03-04 09:45:47 +0530

    JavaCurrencyFormatter

---
+++ src/JavaCurrencyFormatter.java
@@ -0,0 +1,29 @@
+//Mindula Dilthushan
+//Hacker Rank - Java
+import java.util.*;
+import java.text.*;
+
+public class JavaCurrencyFormatter {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+
+        // Write your code here.
+
+        NumberFormat n = NumberFormat.getCurrencyInstance(Locale.US);
+        String us = n.format(payment);
+        NumberFormat n1 = NumberFormat.getCurrencyInstance(new Locale("en", "IN"));
+        String india = n1.format(payment);
+        NumberFormat n2 = NumberFormat.getCurrencyInstance(Locale.CHINA);
+        String china = n2.format(payment);
+        NumberFormat n3 = NumberFormat.getCurrencyInstance(Locale.FRANCE);
+        String france = n3.format(payment);
+
+        System.out.println("US: " + us);
+        System.out.println("India: " + india);
+        System.out.println("China: " + china);
+        System.out.println("France: " + france);
+    }
+}
