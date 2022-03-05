bc59da45433fd55927832a47ab4d3d8c87c210e5
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2022-03-05 13:50:12 +0530

    Java Substring Comparisons

---
+++ src/JavaSubstringComparisons.java
@@ -0,0 +1,34 @@
+//Mindula Dilthushan
+//Hacker Rank - Java
+
+import java.util.Scanner;
+
+public class JavaSubstringComparisons {
+
+    public static String getSmallestAndLargest(String s, int k) {
+        String smallest = "";
+        String largest = "";
+        // Complete the function
+        // 'smallest' must be the lexicographically smallest substring of length 'k'
+        // 'largest' must be the lexicographically largest substring of length 'k'
+        smallest = largest = s.substring(0, k);
+
+        for (int i = 1; i < s.length() - k + 1; i++) {
+            String substr = s.substring(i, i + k);
+            if (smallest.compareTo(substr) > 0)
+                smallest = substr;
+            if (largest.compareTo(substr) < 0)
+                largest = substr;
+        }
+        return smallest + "\n" + largest;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        int k = scan.nextInt();
+        scan.close();
+        System.out.println(getSmallestAndLargest(s, k));
+    }
+}
