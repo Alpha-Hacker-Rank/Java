189f29dea51e6c4b1fb56e751ca9d4d5083a53a4
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2022-03-20 18:21:22 +0530

    Java-1D-Array-Part2

---
+++ src/Java1DArrayPart2.java
@@ -0,0 +1,44 @@
+//Mindula Dilthushan
+//Hacker Rank - Java
+import java.util.*;
+
+public class Java1DArrayPart2 {
+
+    public static boolean canWin(int leap, int[] game) {
+        // Return true if you can win the game; otherwise, return false.
+        return canWin(leap, game, 0);
+    }
+
+    public static boolean canWin(int leap, int[] g, int i) {
+        if (i < 0 || g[i] == 1) {
+            return false;
+        }
+        if (i + leap >= g.length || i == g.length - 1) {
+            return true;
+        }
+        g[i] = 1;
+
+        return (
+                canWin(leap, g, i + 1) ||
+                        canWin(leap, g, i + leap) ||
+                        canWin(leap, g, i - 1)
+        );
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int q = scan.nextInt();
+        while (q-- > 0) {
+            int n = scan.nextInt();
+            int leap = scan.nextInt();
+
+            int[] game = new int[n];
+            for (int i = 0; i < n; i++) {
+                game[i] = scan.nextInt();
+            }
+
+            System.out.println((canWin(leap, game)) ? "YES" : "NO");
+        }
+        scan.close();
+    }
+}
